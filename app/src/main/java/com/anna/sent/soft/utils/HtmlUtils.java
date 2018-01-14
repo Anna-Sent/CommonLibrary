d8bf38b95f0eefe7a1bd644c595b0d31fb09943c
Anna <anna.sent@gmail.com>
2018-01-15 02:14:03 +0400

    html utils

--- app/src/main/java/com/anna/sent/soft/utils/HtmlUtils.java
+++ app/src/main/java/com/anna/sent/soft/utils/HtmlUtils.java
@@ -5,7 +5,7 @@ import android.text.Spanned;
 
 public class HtmlUtils {
     @SuppressWarnings("deprecation")
-    private static Spanned fromHtml(String html) {
+    public static Spanned fromHtml(String html) {
         Spanned result;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);

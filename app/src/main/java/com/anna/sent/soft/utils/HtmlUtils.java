d5a5f83c38bb666c6aa76a3a09ac6f8f6bbc4832
Anna <anna.sent@gmail.com>
2018-01-15 01:53:19 +0400

    html utils

---
+++ app/src/main/java/com/anna/sent/soft/utils/HtmlUtils.java
@@ -0,0 +1,17 @@
+package com.anna.sent.soft.utils;
+
+import android.text.Html;
+import android.text.Spanned;
+
+public class HtmlUtils {
+    @SuppressWarnings("deprecation")
+    private static Spanned fromHtml(String html) {
+        Spanned result;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            result = Html.fromHtml(html);
+        }
+        return result;
+    }
+}

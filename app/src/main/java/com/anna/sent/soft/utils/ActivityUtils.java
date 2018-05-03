51d9ac1f1f232fa9af2a5ff545911ccd0d2b3c97
Anna Sentyakova <anna.sent@gmail.com>
2018-05-03 13:57:04 +0400

    version 1026

--- app/src/main/java/com/anna/sent/soft/utils/ActivityUtils.java
+++
@@ -1,9 +0,0 @@
-package com.anna.sent.soft.utils;
-
-import android.app.Activity;
-
-public class ActivityUtils {
-    public static void restartActivity(Activity activity) {
-        activity.recreate();
-    }
-}
